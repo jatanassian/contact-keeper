405b5c2535e6f76274f7f7c53194fa22609d95b9
Julien Atanassian <julien.atanassian@gmail.com>
2020-04-03 14:04:29 -0400

    create route for homepage

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to Contact Keeper API' }));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
