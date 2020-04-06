9c458e3f2078a5da58e0248772710869c3be3c45
Julien Atanassian <julien.atanassian@gmail.com>
2020-04-06 08:22:25 -0400

    register new user to the database

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -17,13 +18,40 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+      // Check if the user already exists through the email
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists ' });
+      }
+
+      // Create a new instance of User
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Hash the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save in the database
+      await user.save();
+
+      res.send('User saved');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
