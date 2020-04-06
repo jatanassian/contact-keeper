8e2bb7292c61ccf10b4c9124e75215673d0c7ad5
Julien Atanassian <julien.atanassian@gmail.com>
2020-04-06 08:53:41 -0400

    check email and password

--- routes/auth.js
+++ routes/auth.js
@@ -28,6 +28,24 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { email, password } = req.body;
+
+    try {
+      // Check the email
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid credentials ' });
+      }
+      // Check password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid credentials' });
+      }
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
