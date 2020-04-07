4c304cf74ef7a5f88f37cfd47e1f45a62f058054
Julien Atanassian <julien.atanassian@gmail.com>
2020-04-07 09:52:12 -0400

    show email and phone and add buttons

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -9,6 +9,7 @@ const ContactItem = ({ contact }) => {
       <h3 className='text-primary text-left'>
         {name}{' '}
         <span
+          style={{ float: 'right' }}
           className={
             'badge ' +
             (type === 'professional' ? 'badge-success' : 'badge-primary')
@@ -17,6 +18,20 @@ const ContactItem = ({ contact }) => {
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open'></i> {email}
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'></i> {phone}
+          </li>
+        )}
+      </ul>
+      <button className='btn btn-dark btn-sm'>Edit</button>
+      <button className='btn btn-danger btn-sm'>Delete</button>
     </div>
   );
 };
