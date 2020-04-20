a475748dcecc9c1a544d38543a8aae554ee4bdc7
Julien Atanassian <julien.atanassian@gmail.com>
2020-04-19 22:55:50 -0400

    create Login component and add a onSubmit function

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -5,19 +5,25 @@ const Register = () => {
     name: '',
     email: '',
     password: '',
-    password2,
+    password2: '',
   });
 
   const { name, email, password, password2 } = user;
 
   const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Register submit');
+  };
+
   return (
     <div className='form-container'>
       <h1>
         Account <span className='text-primary'>Register</span>
       </h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor='name'>Name</label>
           <input type='text' name='name' value={name} onChange={onChange} />
