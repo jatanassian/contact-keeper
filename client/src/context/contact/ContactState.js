8f2111104a6d72f8f9b3237ba298e50ab5f9c9e4
Julien Atanassian <julien.atanassian@gmail.com>
2020-05-09 23:59:05 -0400

    ad getContacts in the Contacts component and add a spinner

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -29,12 +29,16 @@ const ContactState = (props) => {
   const getContacts = async () => {
     try {
       const res = await axios.get('/api/contacts');
+
       dispatch({
         type: GET_CONTACTS,
         payload: res.data,
       });
     } catch (err) {
-      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
     }
   };
 
